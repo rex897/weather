8fa34ccf19cda334dd67891b3ac5f4c830d3a0f9
rex897 <kostya77797@gmail.com>
2017-07-13 15:44:25 +0500

    исправил тест WeatherTest.java

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -9,9 +9,18 @@ public class WeatherTest {
     @Test
     public void ftoC() throws Exception {
         Weather weather = new Weather();
-        int f = 90;
-        int c = (( f - 32) * 5) / 9;
-        assertEquals(c, weather.FtoC(f));
+        int c1 = 0;
+        int c2 = 15;
+        int c3 = 25;
+        int c4 = 100;
+        int f1 = 32;
+        int f2 = 59;
+        int f3 = 77;
+        int f4 = 212;
+        assertEquals(c1, weather.FtoC(f1));
+        assertEquals(c2, weather.FtoC(f2));
+        assertEquals(c3, weather.FtoC(f3));
+        assertEquals(c4, weather.FtoC(f4));
     }
 
 }
